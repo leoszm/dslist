a9dc70242962b0090bfd0dcd56d2eb17c13f229b
leoszm <leonardosousa60@gmail.com>
2025-08-29 21:56:46 -0300

    ORM and annotations

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,9 +1,25 @@
 package com.devsuperior.dslist.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//Configura que a classe java seja equivalente a uma tabela do banco relacional
+@Entity
+@Table(name = "tb_game")
 public class Game {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String title;
+	
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platform;
@@ -91,6 +107,24 @@ public class Game {
 	
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+			
 	}
 
 }
